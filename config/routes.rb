95281d8e0a3fdeb01d1ef226aa48c78d2d7b765d
Mateo Olarte <mateo.olarte8@gmail.com>
2018-01-09 13:41:56 -0500

    [fixed] change namespace of api products controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      namespace :products do
-        get '/' => 'products#index'
-      end  
+      get '/products' => 'products#index'
     end  
   end  
 
