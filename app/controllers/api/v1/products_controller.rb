ad54294a63ad17801334e18088ab085c777f2cde
Mateo Olarte <mateo.olarte8@gmail.com>
2018-01-09 22:37:33 -0500

    [add] create products with API

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,20 @@
-module Api
-  module V1
-    class ProductsController < ApplicationController
-      def index
-        @products = Product.all
-        render json: @products
-      end
-    end  
-  end  
-end
+class Api::V1::ProductsController < ApplicationController
+
+  def index
+    render json: Product.all
+  end
+
+  def create
+    @product = Product.new(products_params)
+    if @product.save
+      render json: Product.all
+    else
+      render :json => { :errors => @product.errors.full_messages }
+    end
+  end
+
+  private
+  def products_params
+    params.permit(:name, :price)
+  end
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get '/products' => 'products#index'
+      resources :products
     end  
   end  
 
