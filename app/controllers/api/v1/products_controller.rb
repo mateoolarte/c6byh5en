1052a3a287719cdbcaad42df67abd0dcdcab63c9
Mateo Olarte <mateo.olarte8@gmail.com>
2018-01-09 23:57:47 -0500

    [add] update and delete products api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,17 +1,32 @@
 class Api::V1::ProductsController < ApplicationController
-  skip_before_action :verify_authenticity_token  
+  protect_from_forgery with: :null_session 
 
   def index
     render json: Product.all
   end
 
   def create
-    @product = Product.new(products_params)
-    if @product.save
-      render status: 201, json: { product: Product.all } 
+    product = Product.new(products_params)
+    if product.save
+      render json: product, status: 201
     else
-      render status: 422, json: { errors: @product.errors.full_messages }
+      render status: 422, json: { errors: product.errors.full_messages }
     end
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.update(products_params)
+      render json: product
+    else
+      render status: 422, json: { errors: product.errors.full_messages } 
+    end   
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    head :no_content
   end
 
   private
